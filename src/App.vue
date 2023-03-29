d08ec0c15757977a6e9dc648473988db231ca2fa
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-29 15:56:55 +0200

    Creazione del component TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
+<TheHeader />
 
+<main>
+  
+</main>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 
+export default{
+  components: {
+        TheHeader,
+    }
+}
 </script>
 
 <style lang="scss">
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="">
+        <div class="container d-flex align-center">
+            <img id="logo" class="me-3" src="/logo.png" alt="Yu-Gi-Oh logo">
+            <h1>Yu-Gi-Ho Api</h1>
+        </div>
+    </header>
+</template>
+
+
+<script>
+export default{
+    name: 'TheHeader',
+}
+</script>
+
+
+<style scoped lang="scss">
+#logo {
+    height: 3rem;
+}
+</style>
